25cc8d10ea58a50c19de0790dd2b3f77868bc4ff
Anas Yousif <Anasyousif182@gmail.com>
2023-12-01 06:11:42 -0500

    morning push

--- bubbleSort/lessOptimized.js
+++ bubbleSort/lessOptimized.js
@@ -14,16 +14,37 @@
 
 // without issues
 
-function bubbleSort(arr) {
-    for (var i = 0; i < arr.length; i--) {
-      for (var j = 0; j < i -1 ; j++) {
-        if (arr[j] > arr[j + 1]) {
+// function bubbleSort(arr) {
+//     for (var i = 0; i < arr.length; i--) {
+//       for (var j = 0; j < i -1 ; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//         }
+//         var temp = arr[j]
+//         arr[j] = arr[j+1];
+//         arr[j+1] = temp;
+//       }
+//     }
+//     return arr
+//   }
+//   bubbleSort([37,45,29,8])
+
+// with NoSwaps
+
+function bubbleSort(arr){
+    var noSwaps;
+    for(var i = arr.length; i > 0; i--){
+      noSwaps = true;
+      for(var j = 0; j < i - 1; j++){
+        if(arr[j] > arr[j+1]){
+          var temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          noSwaps = false;         
         }
-        var temp = arr[j]
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
       }
+      if(noSwaps) break;
     }
-    return arr
+    return arr;
   }
-  bubbleSort([37,45,29,8])
+  
+  bubbleSort([8,1,2,3,4,5,6,7]);
