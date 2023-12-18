47e166fb6357124a580aedace2674bcbb5981979
Anas Yousif <Anasyousif182@gmail.com>
2023-12-17 20:45:33 -0500

    change

--- dailycode/Traffic_light.js
+++ dailycode/Traffic_light.js
@@ -11,3 +11,17 @@ function updateLight(current) {
   
   
   }
+
+  function updateLight2(current) {
+  
+    // function that returns green , yellow , red , green based on movement
+    if (current === 'green' ){
+      return 'yellow'
+    } if (current === 'yellow' ){
+      return 'red'
+    } else {
+      return 'green'
+    }
+  
+  
+  }
