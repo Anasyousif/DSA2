3668ba6a0e1d8f033e9a867c0c8f053740cd0f17
Anas Yousif <Anasyousif182@gmail.com>
2024-01-08 06:11:21 -0500

    today's push

---
+++ dailycode/printing_numbers.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace my {
+class Printing
+
+ {static void Main(string[] args) {
+   
+   int x = 1;
+   int y   = 101;
+   int z = 1001;
+   
+   Console.WriteLine(x);
+   Console.WriteLine(y);
+   Console.WriteLine(z);
+   
+ }
+
+    }
+    }
