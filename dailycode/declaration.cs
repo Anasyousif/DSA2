b406e5fb2bb939998a23231e573a300629691cc6
Anas Yousif <Anasyousif182@gmail.com>
2024-01-11 17:12:18 -0500

    push

---
+++ dailycode/declaration.cs
@@ -0,0 +1,11 @@
+using System; 
+class Declaration
+{
+    static void Main(){
+    var = char 
+    }
+}
+
+// Declare a variable of type char and assign it as a value the character, 
+// which has Unicode code, 72 (use the Windows calculator in order to find 
+// hexadecimal representation of 72).
