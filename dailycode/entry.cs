7034e33c53ad51f0d4dffb41f9f8e745cbe872d7
Anas Yousif <Anasyousif182@gmail.com>
2024-01-15 22:02:10 -0500

    today's push

---
+++ dailycode/entry.cs
@@ -0,0 +1,21 @@
+namespace EmployeeData
+{
+    internal class Employee
+    {
+        static void Main()
+        {
+            string firstName = "";
+            string LasttName = "";
+            char  gender = 'M';
+            sbyte age = 127;
+            int UniqueEmployeeNumber = 27569999;
+            Console.WriteLine(firstName);
+            Console.WriteLine(LasttName);
+            Console.WriteLine(gender);
+            Console.WriteLine(age);
+            Console.WriteLine(UniqueEmployeeNumber);
+
+
+        }
+    }
+}
