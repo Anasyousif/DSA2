2f9b0db7b738fefc824e341e6052cbac05f177b3
Anas Yousif <Anasyousif182@gmail.com>
2024-01-16 19:17:44 -0500

    today's push

---
+++ dailycode/random.cs
@@ -0,0 +1,6 @@
+IEnumerable<int> numbers = [10, 20, 30];
+IList<int> indexable = numbers as IList<int>;
+if (indexable != null)
+{
+    Console.WriteLine(indexable[0] + indexable[indexable.Count - 1]);  // output: 40
+}
