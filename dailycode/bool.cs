249912cbf95e18fd3719319170a4aa6aef91f8c8
Anas Yousif <Anasyousif182@gmail.com>
2024-01-18 20:44:15 -0500

    today's push

---
+++ dailycode/bool.cs
@@ -0,0 +1,11 @@
+namespace IsMalebool
+{
+    internal class IsMaleBool
+    {
+        static void Main()
+        {
+            bool ismale = true;
+            Console.WriteLine(ismale);
+        }
+    }
+}
