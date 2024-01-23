aab18d584c42b88ed4b3a7be2c34656d559d76f3
Anas Yousif <Anasyousif182@gmail.com>
2024-01-22 19:58:24 -0500

    today's push

---
+++ dailycode/EvenorOdd.cs
@@ -0,0 +1,14 @@
+namespace OddorEeven
+{
+    internal class OddorEeven
+    {
+        static void Main(string[] args)
+
+        {
+            int a = 5%2;
+            int b = 10 % 2;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
