51dd6e38ca0ed11e2ee1b96bc1f68db5618dfcd5
Anas Yousif <Anasyousif182@gmail.com>
2024-01-25 08:24:30 -0500

    today's input

---
+++ dailycode/printingnums.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel;
+
+namespace NumbersWithVirtualColumns
+{
+    internal class NumberswithVirtualColumns
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("{0,-10}",9);
+            Console.WriteLine("{0,-10}", 12.6);
+            Console.WriteLine("{0,-10}", -12.7);
+        }
+    }
+}
+
