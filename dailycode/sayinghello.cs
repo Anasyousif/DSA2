9be1a0a2ea4db3d3f1950167ce184af38962b81d
Anas Yousif <Anasyousif182@gmail.com>
2024-01-29 20:51:45 -0500

    today's push

---
+++ dailycode/sayinghello.cs
@@ -0,0 +1,16 @@
+namespace PrintingFirst100NumbersinFinb
+{
+    internal class PrintingFirst100NumbersinFinb
+    {
+        static void Main()
+        {
+            int a = 0; 
+            int b = a+0;
+            for (int i = 0; i < 233; i+=a)
+            {
+                Console.WriteLine(b);
+
+            }
+        }
+    }
+}
