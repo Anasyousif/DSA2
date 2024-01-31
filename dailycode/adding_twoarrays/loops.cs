764b5ac4c75d970234c83bcbe6a08402947c0157
Anas Yousif <Anasyousif182@gmail.com>
2024-01-30 20:15:28 -0500

    today's push

---
+++ dailycode/adding_twoarrays/loops.cs
@@ -0,0 +1,14 @@
+namespace ReadingandPrintingallNumbers
+{
+    internal class ReadingandPrinting
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
