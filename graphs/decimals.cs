5733f7088dfc68400b86374a42d4f7e821ad9594
Anas Yousif <Anasyousif182@gmail.com>
2024-03-08 21:47:23 -0500

    Frdiay push

---
+++ graphs/decimals.cs
@@ -0,0 +1,14 @@
+namespace DivisibleBy5or7
+{
+    internal class Divisibleby5or7
+    {
+        static void Main(string[] args)
+        {
+            int b = 7;
+            int c = 5;
+            
+            Console.WriteLine(35/c);
+            Console.WriteLine(35 / b);
+        }
+    }
+}
